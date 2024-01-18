220b208dfee86b0fc5aa34a46a6e7347430819ec
Christian Burau <christian.burau@icloud.com>
2024-01-18 07:59:20 +0100

    Update

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Verwende das offizielle Python-Image als Basis
+FROM python:3.8-slim
+
+# Setze das Arbeitsverzeichnis im Container
+WORKDIR /app
+
+# Kopiere die Python-Skriptdateien in das Arbeitsverzeichnis
+#COPY app.py .
+#COPY templates templates
+
+# Installiere die erforderlichen Python-Pakete
+RUN pip install Flask gunicorn
+
+# Öffne den Port 5000
+EXPOSE 80
+
+# Starte den Gunicorn-Webserver beim Containerstart
+CMD ["gunicorn", "-b", "0.0.0.0:80", "app:app"]
---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+app1 = Flask(__name__)
+
+def mgdl_to_mmoll(mgdl_value):
+    conversion_factor = 0.0555
+    mmoll_value = mgdl_value * conversion_factor
+    return mmoll_value
+
+def mmoll_to_mgdl(mmoll_value):
+    conversion_factor = 18.02
+    mgdl_value = mmoll_value * conversion_factor
+    return mgdl_value
+
+@app1.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+
+    if request.method == 'POST':
+        input_value = float(request.form['input_value'].replace(',', '.'))  # Ersetze das Komma durch einen Punkt
+        conversion_type = request.form['conversion_type']
+
+        if conversion_type == 'mgdl_to_mmoll':
+            result = f"{input_value} mg/dL entsprechen {mgdl_to_mmoll(input_value):,.2f} mmol/L."
+        elif conversion_type == 'mmoll_to_mgdl':
+            result = f"{input_value} mmol/L entsprechen {mmoll_to_mgdl(input_value):,.2f} mg/dL."
+
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app1.run(debug=True, port=80)
